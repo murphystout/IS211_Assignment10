eaced390feead56e5ae149c3861253e3b8151b7e
murphystout <hayes_michael_d@yahoo.com>
2015-04-16 11:37:06 -0400

    music.sql

---
+++ music.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE artists (
+id INTEGER PRIMARY KEY,
+first_name TEXT,
+last_name TEXT
+);
+
+CREATE TABLE albums (
+id INTEGER PRIMARY KEY,
+artist_id INTEGER NOT NULL,
+album_name TEXT
+);
+
+CREATE TABLE songs (
+id INTEGER PRIMARY KEY,
+album_id INTEGER NOT NULL,
+song_name TEXT NOT NULL,
+track_number INTEGER,
+num_seconds INTEGER
+);
